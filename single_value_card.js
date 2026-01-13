5dafcf7955e6b5e9d85db6aaf81a2a18806a1ea9
Mikey Cannon <mikeycandesign@gmail.com>
2026-01-13 15:07:55 +0000

    Update single_value_card.js

--- single_value_card.js
+++ single_value_card.js
@@ -63,6 +63,13 @@ looker.plugins.visualizations.add({
 
   // --- 2. SETUP ---
   create: function(element, config) {
+    // Initial setup - we insert the container here.
+    // If Looker clears 'element' later, updateAsync will handle it.
+    this._addBaseStructure(element);
+  },
+  
+  // Helper to add the style and container
+  _addBaseStructure: function(element) {
     element.innerHTML = `
       <style>
         /* IMPORT NUNITO FONT */
@@ -184,14 +191,21 @@ looker.plugins.visualizations.add({
 
       </style>
       
-      <div id="vis-container" style="padding:0; width:100%; height:100%; box-sizing:border-box; background:#f8f9fa; display: flex; align-items: flex-start;">
+      <div class="vis-container" style="padding:0; width:100%; height:100%; box-sizing:border-box; background:#f8f9fa; display: flex; align-items: flex-start;">
       </div>
     `;
   },
 
   // --- 3. RENDER ---
   updateAsync: function(data, element, config, queryResponse, details, done) {
-    var container = element.querySelector("#vis-container");
+    
+    // --- SAFETY CHECK: REBUILD CONTAINER IF MISSING ---
+    // If Looker cleared the element (common on resize), re-inject the base structure.
+    var container = element.querySelector(".vis-container");
+    if (!container) {
+      this._addBaseStructure(element);
+      container = element.querySelector(".vis-container");
+    }
 
     if (!data || data.length === 0) {
       container.innerHTML = `<div style="color:#999; padding:20px;">No Data</div>`;
@@ -232,7 +246,7 @@ looker.plugins.visualizations.add({
       "cash": '<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />',
       "users": '<path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />',
       "chart": '<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M6 16.5V2.25m0 14.25h12A2.25 2.25 0 0020.25 14.25V2.25M6 2.25h12M9 13.5l3-3m0 0l3 3m-3-3v7.5" />',
-      "calendar": '<path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />',
+      "calendar": '<path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />',
       "home": '<path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />'
     };
 
@@ -281,6 +295,7 @@ looker.plugins.visualizations.add({
       </div>
     `;
 
+    // Safe injection
     container.innerHTML = html;
 
     // --- ADD DRILL CLICK LISTENER ---
